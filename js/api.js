1d74a9f17801370e88b30bbd087da8706d8410b9
Hiram <hiram0101@gmail.com>
2018-04-11 22:53:49 -0400

    fixed base url

--- js/api.js
+++ js/api.js
@@ -1,5 +1,5 @@
 const api = (function() {
-  const BASE_URL = `https://forgiv.github.io/todo-list-react/`
+  const BASE_URL = `https://forgiv.github.io/todo-list-react`
   const BOOKMARKS_URL = `${BASE_URL}/bookmarks`
 
   getBookmarks = (callback) => {
