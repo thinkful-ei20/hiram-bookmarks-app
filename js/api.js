351720f65fad3788cd47740bf32ed2a7e2ba8a98
Hiram <hiram0101@gmail.com>
2018-04-11 21:00:43 -0400

    added api module with api url and method stubs

---
+++ js/api.js
@@ -0,0 +1,20 @@
+const api = (function() {
+  const BASE_URL = `https://forgiv.github.io/todo-list-react/`
+  const BOOKMARKS_URL = `${BASE_URL}/bookmarks`
+
+  getBookmarks = () => {
+
+  }
+
+  addBookmark = () => {
+
+  }
+
+  updateBookmark = () => {
+
+  }
+
+  deleteBookmark = () => {
+    
+  }
+}())
